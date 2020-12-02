5e571d64b2894c73afd34b9d97d4f869e38bf313
이준혁 <j005580@naver.com>
2020-12-02 20:17:03 +0900

    backend-dev

---
+++ src/main/java/com/musinsa/watcher/WatcherApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatcherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WatcherApplication.class, args);
+  }
+
+}
