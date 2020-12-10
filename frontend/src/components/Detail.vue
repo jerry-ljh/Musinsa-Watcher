913585a7a5fba49dff25644299ac30fdd81d9281
이준혁 <j005580@naver.com>
2020-12-11 01:19:12 +0900

    과거 실구매 최저가, 평균가 마크업 추가

--- frontend/src/components/Detail.vue
+++ frontend/src/components/Detail.vue
@@ -25,9 +25,10 @@
                     <div style="margin-bottom: 10px">
                         <b-icon icon="trophy"></b-icon>
                         Ranking :
-                        {{lastPrice.rank}}위 
+                        {{lastPrice.rank}}위
                         <small class="text-muted" style="color :#b2b2b2;">
-                              * {{lastPrice.createdDate}}일 기준</small>
+                            *
+                            {{lastPrice.createdDate}}일 기준</small>
                     </div>
                     <div style=" margin-bottom: 10px">
                         <h3 style="text-decoration: line-through; color :#b2b2b2 ">
@@ -41,7 +42,8 @@
                     </div>
                     <div style=" margin-bottom: 10px">
                         <h3>
-                            {{numberToPrice(lastPrice.price)}}원</h3>
+                            {{numberToPrice(lastPrice.price)}}원
+                        </h3>
                     </div>
                     <div v-if="lastPrice.coupon==0" style="color :#b2b2b2;">
                         <b-icon icon="sticky"></b-icon>
@@ -50,6 +52,19 @@
                         <b-icon icon="sticky"></b-icon>
                         추가 쿠폰 :
                         {{numberToPrice(lastPrice.coupon)}}원</div>
+                    <span style="color:rgb(234 7 7)">과거 실구매 최저가 :
+                        {{numberToPrice(Math.min.apply(null, this.realPriceList.slice(0, this.realPriceList.length - 1)))}}원</span><br/>
+                    <span style="color:rgb(234 7 7)">과거 실구매 평균가 :
+                        {{numberToPrice(computeAvg(this.realPriceList))}}원</span><br/>
+                    <span
+                        style="color:rgb(234 7 7)"
+                        v-if="computeOrder(this.realPriceList)==0">
+                        <strong>오늘은 역대 가장 낮은 가격입니다.</strong>
+                    </span>
+                    <span
+                        style="color:rgb(234 7 7) "
+                        v-if="computeOrder(this.realPriceList)!=0">오늘은 역대 상위
+                        {{numberToPrice(computeOrder(this.realPriceList))}}%로 낮은 가격입니다.</span><br/>
                     <div style="vertical-align : middle">
                         <b-form-rating
                             id="rating-inline"
@@ -63,7 +78,7 @@
                     <line-chart ref="chart" :datacollection="datacollection" :options="options"></line-chart>
                     <div style=" margin-top: 10px">
                         <small class="text-muted">
-                            * MUSINSA WATCHER는 카테고리별 1~3,000위 까지의 자료를 수집합니다. 순위 미진입시 데이터가 없을 수 있습니다.
+                            * MUSINSA WATCHER는 카테고리별 상위 9000위의 데이터를 수집합니다. 순위 미진입시 데이터가 없을 수 있습니다.
                         </small>
                     </div>
                 </b-col>
@@ -111,21 +126,25 @@
         },
         methods: {
             generatePriceData() {
-                for (var i = this.prices.length - 1; i >= 0; i--) {
+                var end = this
+                    .prices
+                    .length
+                    for (var i = 0; i < end; i++) {
+                        this
+                            .priceList
+                            .push(this.prices[end - 1 - i].price)
+                        this
+                            .couponList
+                            .push(this.prices[end - 1 - i].coupon)
+                        this
+                            .realPriceList
+                            .push(this.prices[end - 1 - i].price + this.prices[end - 1 - i].coupon)
+                        this
+                            .dateList
+                            .push(this.prices[end - 1 - i].createdDate)
+                    }
                     this
-                        .priceList
-                        .push(this.prices[i].price)
-                    this
-                        .couponList
-                        .push(this.prices[i].coupon)
-                    this
-                        .realPriceList
-                        .push(this.prices[i].price + this.prices[i].coupon)
-                    this
-                        .dateList
-                        .push(this.prices[i].createdDate)
-                }
-                this.chartRender();
+                    .chartRender();
             },
             chartRender() {
                 this
@@ -139,18 +158,10 @@
                             label: '가격',
                             pointBackgroundColor: 'white',
                             borderWidth: 2,
-                            borderColor: '#249EBF',
-                            pointBorderColor: '#249EBF',
+                            borderColor: 'rgba(255, 99, 132, 1)',
+                            pointBorderColor: 'rgba(255, 99, 132, 1)',
                             fill: false,
                             data: this.priceList
-                        }, {
-                            label: '쿠폰',
-                            pointBackgroundColor: 'white',
-                            borderWidth: 2,
-                            borderColor: '#b2b2b2',
-                            pointBorderColor: '#b2b2b2',
-                            fill: false,
-                            data: this.couponList
                         }, {
                             label: '실 구매가',
                             pointBackgroundColor: 'white',
@@ -159,6 +170,14 @@
                             pointBorderColor: 'blue',
                             fill: false,
                             data: this.realPriceList
+                        }, {
+                            label: '쿠폰',
+                            pointBackgroundColor: 'white',
+                            borderWidth: 2,
+                            borderColor: 'rgba(255, 159, 64, 1)',
+                            pointBorderColor: 'rgba(255, 159, 64, 1)',
+                            fill: false,
+                            data: this.couponList
                         }
                     ]
                 },
@@ -200,6 +219,23 @@
                     return number
                 }
                 return number.toLocaleString();
+            },
+            computeAvg(list) {
+                var sum = 0;
+                for (var i = 0; i < list.length - 1; i++) {
+                    sum += parseInt(list[i], 10);
+                }
+                var avg = sum / (list.length-1);
+                return Math.ceil(avg)
+            },
+            computeOrder(list) {
+                var count = 0;
+                for (var i = 0; i < list.length - 1; i++) {
+                    if (list[list.length - 1] >= list[i]) {
+                        count += 1
+                    }
+                }
+                return Math.ceil(count / (list.length-1) * 100)
             }
         },
         created() {
