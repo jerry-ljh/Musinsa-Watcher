e0588168455f5971d91bd0c4496ea2b32d56ab77
이준혁 <j005580@naver.com>
2020-12-26 17:46:44 +0900

    [Fix] 실행 중인 포트가 아닌 PID를 기준으로 로직이 실행되던 문제 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,11 +11,11 @@ if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  if [ ${IDLE_PID}=8081 ]
+  if [ ${IDLE_PORT}=8081 ]
   then
     echo "> port : $IDLE_PID로 실행중인 real1을 컨테이너 종료합니다."
     docker-compose kill real1 
-  elif [ ${IDLE_PID}=8082 ]
+  elif [ ${IDLE_PORT}=8082 ]
   then
     echo "> port : $IDLE_PID로 실행중인 real2을 컨테이너 종료합니다."
     docker-compose kill real2 
