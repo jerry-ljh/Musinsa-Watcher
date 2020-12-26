334e8a0eac1ca949ac99f3d302873d4be295183b
이준혁 <j005580@naver.com>
2020-12-26 18:25:12 +0900

    [Fix] shell script의 값 비교를 잘못된 기호로 적용하던 문제 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,14 +11,14 @@ if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  if [ ${IDLE_PORT}=8081 ]
+  if [ ${IDLE_PORT} -eq 8081 ]
   then
     echo "> port : $IDLE_PID로 실행중인 real1을 컨테이너 종료합니다."
-    docker-compose kill real1 
-  elif [ ${IDLE_PORT}=8082 ]
+    #docker-compose kill real1
+  elif [ ${IDLE_PORT} -eq 8082 ]
   then
     echo "> port : $IDLE_PID로 실행중인 real2을 컨테이너 종료합니다."
-    docker-compose kill real2 
+    #docker-compose kill real2
   fi
   sleep 10
 fi
