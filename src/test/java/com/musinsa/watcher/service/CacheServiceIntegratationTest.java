c1f009558389417a3dfea1f29cee30d90b706531
이준혁 <j005580@naver.com>
2021-01-12 19:15:27 +0900

    [Add] 캐시 테스트 코드 작성

---
+++ src/test/java/com/musinsa/watcher/service/CacheServiceIntegratationTest.java
@@ -0,0 +1,101 @@
+package com.musinsa.watcher.service;
+
+import static org.junit.Assert.assertEquals;
+
+import com.musinsa.watcher.domain.product.Product;
+import com.musinsa.watcher.domain.product.ProductRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CacheServiceIntegratationTest {
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  @Autowired
+  private CacheService cacheService;
+
+  @Autowired
+  private CacheManager cacheManager;
+
+  @Before
+  public void clean() {
+    cacheManager.getCache("productCache").clear();
+    productRepository.deleteAll();
+  }
+
+  @Test
+  @DisplayName("캐시를 사용하여 호출한다.")
+  public void callCache() {
+    LocalDateTime localDateTime1 = LocalDateTime.now();
+    LocalDateTime localDateTime2 = localDateTime1.plusDays(3);
+    productRepository.save(Product.builder()
+        .productId(1)
+        .productName("고급 신발")
+        .brand("고급 브랜드")
+        .category("001")
+        .img("http://cdn.img?id=10000_125.jpg")
+        .modifiedDate(localDateTime1)
+        .build());
+    cacheService.updateCacheByDate();
+    productRepository.save(Product.builder()
+        .productId(2)
+        .productName("고급 신발")
+        .brand("고급 브랜드")
+        .category("001")
+        .img("http://cdn.img?id=10000_125.jpg")
+        .modifiedDate(localDateTime2)
+        .build());
+
+    cacheService.getLastUpdatedDate();
+    LocalDate localDate = cacheService.getLastUpdatedDate();
+
+    assertEquals(localDate, localDateTime1.toLocalDate());
+  }
+
+  @Test
+  @DisplayName("캐시를 초기화 한다.")
+  public void clearCache() {
+    //given
+    LocalDateTime localDateTime1 = LocalDateTime.now();
+    LocalDateTime localDateTime2 = localDateTime1.plusMinutes(1L);
+    productRepository.save(Product.builder()
+        .productId(1)
+        .productName("고급 신발")
+        .brand("고급 브랜드")
+        .category("001")
+        .img("http://cdn.img?id=10000_125.jpg")
+        .modifiedDate(localDateTime1)
+        .build());
+    //when
+    cacheService.updateCacheByDate();
+    //given
+    productRepository.save(Product.builder()
+        .productId(2)
+        .productName("고급 신발")
+        .brand("고급 브랜드")
+        .category("001")
+        .img("http://cdn.img?id=10000_125.jpg")
+        .modifiedDate(localDateTime2)
+        .build());
+    //when
+    cacheService.updateCacheByDate();
+    LocalDateTime cachedLocalDateTime = (LocalDateTime) cacheManager.getCache("productCache")
+        .get("current date").get();
+
+    //then
+    assertEquals(cachedLocalDateTime, localDateTime2);
+  }
+}
