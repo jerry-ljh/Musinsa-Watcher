69bc655b557a035c408fe63eed2feedcf7ab20c4
이준혁 <j005580@naver.com>
2021-04-18 04:22:15 +0900

    [Update] 쿼리 수정에 따른 DTO변경

--- src/main/java/com/musinsa/watcher/web/dto/MinimumPriceProductDto.java
+++ src/main/java/com/musinsa/watcher/web/dto/MinimumPriceProductDto.java
@@ -1,16 +1,10 @@
 package com.musinsa.watcher.web.dto;
 
+import com.musinsa.watcher.domain.price.TodayMinimumPriceProduct;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 
 @Getter
 public class MinimumPriceProductDto implements Serializable {
@@ -20,47 +14,18 @@ public class MinimumPriceProductDto implements Serializable {
   private String brand;
   private String img;
   private LocalDate modifiedDate;
-  private int today_price;
+  private int todayPrice;
   private int avgPrice;
 
   @Builder
-  public MinimumPriceProductDto(int productId, String productName, String brand,
-      String img, Timestamp modifiedDate, int today_price, int avgPrice) {
-    this.productId = productId;
-    this.productName = productName;
-    this.today_price = today_price;
-    this.avgPrice = avgPrice;
-    this.brand = brand;
-    this.img = img;
-    this.modifiedDate = modifiedDate.toLocalDateTime().toLocalDate();
+  public MinimumPriceProductDto(TodayMinimumPriceProduct entity) {
+    this.productId = entity.getProduct().getProductId();
+    this.productName = entity.getProduct().getProductName();
+    this.todayPrice = entity.getToday_price();
+    this.avgPrice = (int)entity.getAvg_price();
+    this.brand = entity.getProduct().getBrand();
+    this.img = entity.getProduct().getImg();
+    this.modifiedDate = entity.getProduct().getModifiedDate().toLocalDate();
   }
-
-  public static MinimumPriceProductDto convertDto(Object[] object) {
-    return MinimumPriceProductDto
-        .builder()
-        .productId((int) object[0])
-        .productName((String) object[1])
-        .brand((String) object[2])
-        .img((String) object[3])
-        .modifiedDate((Timestamp) object[4])
-        .today_price(((int) object[5]))
-        .avgPrice(((BigDecimal) object[6]).intValue())
-        .build();
-  }
-
-  public static List<MinimumPriceProductDto> convertList(List<Object[]> objectList) {
-    return objectList.stream()
-        .map(product -> MinimumPriceProductDto.convertDto(product))
-        .collect(Collectors.toList());
-  }
-
-  public static Page<MinimumPriceProductDto> convertPage(List<Object[]> objectList,
-      Pageable pageable, long count) {
-    return new PageImpl<MinimumPriceProductDto>(
-        MinimumPriceProductDto.convertList(objectList),
-        pageable,
-        count);
-  }
-
 
 }
