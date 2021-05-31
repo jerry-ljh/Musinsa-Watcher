3a1f9c6734a89132f68fa9648cb78fd8b3930175
이준혁 <j005580@naver.com>
2021-06-01 01:49:35 +0900

    [Fix] 카테고리에 대한 데이터가 없는 경우 key가 없는 문제 수정

--- src/main/java/com/musinsa/watcher/web/dto/ProductCountMapByCategoryDto.java
+++ src/main/java/com/musinsa/watcher/web/dto/ProductCountMapByCategoryDto.java
@@ -1,19 +1,23 @@
 package com.musinsa.watcher.web.dto;
 
-import com.musinsa.watcher.domain.price.dto.ProductCountByCategoryDto;
+import com.musinsa.watcher.domain.product.Category;
+import com.musinsa.watcher.domain.product.dto.ProductCountByCategoryDto;
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
 
 @Getter
-public class ProductCountMapByCategoryDto {
+public class ProductCountMapByCategoryDto implements Serializable {
 
   private final Map<String, Integer> categoryProductMap;
 
   public ProductCountMapByCategoryDto(List<ProductCountByCategoryDto> list) {
     Map<String, Integer> map = new LinkedHashMap<>();
     list.forEach(i -> map.put(i.getCategory(), (int) i.getCount()));
+    Arrays.stream(Category.values()).forEach(i -> map.putIfAbsent(i.getCategory(), 0));
     this.categoryProductMap = map;
   }
 
