7ad370cf1e638a8671877eb191ad437dedbd58b0
이준혁 <j005580@naver.com>
2021-06-04 19:26:27 +0900

    [Update] Key Generator 로직 변경

--- src/main/java/com/musinsa/watcher/config/cache/CustomKeyGenerator.java
+++ src/main/java/com/musinsa/watcher/config/cache/CustomKeyGenerator.java
@@ -2,7 +2,7 @@ package com.musinsa.watcher.config.cache;
 
 import java.lang.reflect.Method;
 import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.data.domain.Pageable;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 
 public class CustomKeyGenerator implements KeyGenerator {
 
@@ -10,23 +10,7 @@ public class CustomKeyGenerator implements KeyGenerator {
   public Object generate(Object target, Method method, Object... params) {
     StringBuilder keyBuilder = new StringBuilder();
     keyBuilder.append(method.getName());
-    if (params.length > 0) {
-      keyBuilder.append(";");
-      for (Object argument : params) {
-        if (argument instanceof Pageable) {
-          Pageable pageable = (Pageable) argument;
-          keyBuilder.append(pageable.getPageNumber());
-          keyBuilder.append(";");
-          keyBuilder.append(pageable.getPageSize());
-          keyBuilder.append(";");
-          keyBuilder.append(pageable.getSort().toString());
-          keyBuilder.append(";");
-          continue;
-        }
-        keyBuilder.append(argument);
-        keyBuilder.append(";");
-      }
-    }
+    keyBuilder.append(SimpleKeyGenerator.generateKey(params));
     return keyBuilder.toString();
   }
 }
